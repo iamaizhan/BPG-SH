7a406d3002250b013cfde05def2c920c5c5bf38b
Aizhan <78554414+iamaizhan@users.noreply.github.com>
2024-05-26 15:26:43 +0200

    Update cdhit_clustering.py

--- cdhit_clustering.py
+++ cdhit_clustering.py
@@ -5,6 +5,15 @@ import subprocess
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def run_cd_hit(input_fasta, output_dir, identity=0.95, coverage=0.99):
+    """
+    Spustí CD-HIT ke klastrování proteinových sekvencí.
+
+    Parametry:
+    input_fasta (str): Cesta ke vstupnímu FASTA souboru obsahujícímu proteinové sekvence.
+    output_dir (str): Adresář pro uložení výstupu CD-HIT.
+    identity (float): Práh identity sekvence pro klastrování.
+    coverage (float): Práh pokrytí sekvence pro klastrování.
+    """
     output_fasta = os.path.join(output_dir, os.path.basename(input_fasta).replace('_proteins', '_cdhit_clusters'))
     cmd = [
         'cd-hit',
@@ -24,17 +33,33 @@ def run_cd_hit(input_fasta, output_dir, identity=0.95, coverage=0.99):
         logging.error(f"CD-HIT klastrování se nezdařilo pro {fasta_id}: {e.stderr}")
 
 def cluster_all_fastas(fasta_dir, output_dir, identity=0.95, coverage=0.99):
+    """
+    Shlukuje všechny FASTA soubory v zadaném adresáři pomocí CD-HIT.
+
+    Parametry:
+    fasta_dir (str): Cesta ke vstupnímu FASTA souboru obsahujícímu proteinové sekvence.
+    output_dir (str): Adresář pro uložení výstupu CD-HIT.
+    identity (float): Práh identity sekvence pro klastrování.
+    coverage (float): Práh pokrytí sekvence pro klastrování.
+    """
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    fasta_files = [os.path.join(fasta_dir, f) for f in os.listdir(fasta_dir) if f.endswith('.fasta')]
+    fasta_files = []
+    for f in os.listdir(fasta_dir):
+        if f.endswith('.fasta'):
+            fasta_files.append(os.path.join(fasta_dir, f))
 
     for fasta_file in fasta_files:
         run_cd_hit(fasta_file, output_dir, identity, coverage)
 
 def main():
-    input_fasta_dir = '/cesta_k/proteins'
-    output_dir = '/cesta_k_vystupni_slozce/clstr'
+    """
+    Hlavní funkce pro analýzu argumentů a spuštění shlukování CD-HIT.
+    """
+    input_fasta_dir = '/cesta/k/souborům/proteins'
+    output_dir = '/cesta/do/výstupního/souboru/clstr'
+    
     cluster_all_fastas(input_fasta_dir, output_dir)
     
 if __name__ == "__main__":
